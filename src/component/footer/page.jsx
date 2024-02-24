9da3d71dee23451adbfab2ff35a721b4dda4193e
dozzymanno <doziem4@gmail.com>
2024-02-24 17:59:32 +0100

    second commit

---
+++ src/component/footer/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css"
+
+export default function Footer(){
+    return(
+<>
+<h1 className={styles.footer}>This is the Footer Note, pay attention</h1>
+<p className={styles.ftext}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eos debitis velit? Dolorum vero culpa maxime blanditiis facilis veritatis cum minus! Est, suscipit praesentium esse saepe explicabo ducimus voluptas fugiat eligendi mollitia asperiores, labore minus dolore tempora expedita eaque aut laboriosam! Expedita blanditiis sapiente similique officiis accusantium culpa voluptate iste hic quasi cum tempora exercitationem nemo ipsum repudiandae veritatis, suscipit id beatae quaerat ad, et vero ullam quo! Minus quae dolore ullam qui fuga minima dolorum quas accusamus voluptatibus repellat similique eum sequi hic vel fugiat optio autem totam ad perspiciatis, facilis iste, expedita veritatis, ipsam delectus! Vel, optio itaque!</p>
+</>
+
+    )
+}
